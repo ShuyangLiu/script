fa37598b53938c6d8a63115e9bb1b6e4e97663ed
ShuyangLiu <liushuyang2009@gmail.com>
2017-09-20 02:29:59 -0400

    add answer key generation and answer comparison

--- grade.sh
+++ grade.sh
@@ -11,11 +11,7 @@ compile() {
     # Ada
     ada_srcs=`find .. -name \*.adb`
     if [ ! -z "$ada_srcs" ]; then
-        # create a new file for results
-        touch ../RESULT_ADA.txt
-
-        # a counter keep track of number of test cases passed
-        passed=0
+        # make new directories for each type of file
         mkdir Ada/ Ada/bin/ Ada/obj/
         for ada_src in ${ada_srcs}
         do
@@ -25,6 +21,12 @@ compile() {
         # Assume there is only one main function in total
         # Compile ada sources
         gnatmake -D ./Ada/obj/ -o ./Ada/bin/Program `ls Ada/*.adb`
+
+        # create a new file for results
+        touch ../RESULT_ADA.txt
+
+        # a counter keep track of number of test cases passed
+        passed=0
 
         while read LINE
         do
@@ -52,6 +54,25 @@ compile() {
         # Assume there is only one main function in total
         # Compile c-sharp sources
         mcs `ls ./CS/*.cs` -out:CS/bin/Program
+
+        # create a new file for results
+        touch ../RESULT_CS.txt
+        # a counter keep track of number of test cases passed
+        passed=0
+        # compare answers and results
+        while read LINE
+        do
+            # save results for each test cases
+            ./CS/bin/Program $LINE > "res_${LINE// /-}"
+            # compare it with answer key
+            if [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}`=="" ]] || [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}_rev`=="" ]] ; then
+                # passed the test case
+                passed=$((passed+1))
+                # append result to result file
+                echo "Case: ${LINE// /-} passed" >> ../RESULT_CS.txt
+            fi
+        done < "$testcasesfile"
+        echo "Total number of passed cases: $passed" >> ../RESULT_CS.txt
     fi
 
     # Haskell
@@ -65,6 +86,25 @@ compile() {
         # Assume there is only one main function in total
         # Compile Haskell sources
         ghc -o ./Haskell/bin/Program `ls ./Haskell/*.hs`
+
+        # create a new file for results
+        touch ../RESULT_HS.txt
+        # a counter keep track of number of test cases passed
+        passed=0
+        # compare answers and results
+        while read LINE
+        do
+            # save results for each test cases
+            ./Haskell/bin/Program $LINE > "res_${LINE// /-}"
+            # compare it with answer key
+            if [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}`=="" ]] || [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}_rev`=="" ]] ; then
+                # passed the test case
+                passed=$((passed+1))
+                # append result to result file
+                echo "Case: ${LINE// /-} passed" >> ../RESULT_HS.txt
+            fi
+        done < "$testcasesfile"
+        echo "Total number of passed cases: $passed" >> ../RESULT_HS.txt
     fi
 
     # Ocaml
@@ -78,6 +118,25 @@ compile() {
         # Assume there is only one main function in total
         # Compile Ocaml sources
         ocamlopt -o Ocaml/bin/Program `ls ./Ocaml/*.ml`
+
+        # create a new file for results
+        touch ../RESULT_ML.txt
+        # a counter keep track of number of test cases passed
+        passed=0
+        # compare answers and results
+        while read LINE
+        do
+            # save results for each test cases
+            ./Ocaml/bin/Program $LINE > "res_${LINE// /-}"
+            # compare it with answer key
+            if [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}`=="" ]] || [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}_rev`=="" ]] ; then
+                # passed the test case
+                passed=$((passed+1))
+                # append result to result file
+                echo "Case: ${LINE// /-} passed" >> ../RESULT_ML.txt
+            fi
+        done < "$testcasesfile"
+        echo "Total number of passed cases: $passed" >> ../RESULT_ML.txt
     fi
 
     # Swift
@@ -91,6 +150,25 @@ compile() {
         # Assume there is only one main function in total
         # Compile Swift sources
         swiftc -o Swift/bin/Program `ls ./Swift/*.swift`
+
+        # create a new file for results
+        touch ../RESULT_SW.txt
+        # a counter keep track of number of test cases passed
+        passed=0
+        # compare answers and results
+        while read LINE
+        do
+            # save results for each test cases
+            ./Swift/bin/Program $LINE > "res_${LINE// /-}"
+            # compare it with answer key
+            if [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}`=="" ]] || [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}_rev`=="" ]] ; then
+                # passed the test case
+                passed=$((passed+1))
+                # append result to result file
+                echo "Case: ${LINE// /-} passed" >> ../RESULT_SW.txt
+            fi
+        done < "$testcasesfile"
+        echo "Total number of passed cases: $passed" >> ../RESULT_SW.txt
     fi
 
     # Rust
@@ -104,6 +182,25 @@ compile() {
         # Assume there is only one main function in total
         # Compile Rust sources
         rustc -o Rust/bin/Program `ls ./Rust/*.rs`
+
+        # create a new file for results
+        touch ../RESULT_RS.txt
+        # a counter keep track of number of test cases passed
+        passed=0
+        # compare answers and results
+        while read LINE
+        do
+            # save results for each test cases
+            ./Rust/bin/Program $LINE > "res_${LINE// /-}"
+            # compare it with answer key
+            if [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}`=="" ]] || [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}_rev`=="" ]] ; then
+                # passed the test case
+                passed=$((passed+1))
+                # append result to result file
+                echo "Case: ${LINE// /-} passed" >> ../RESULT_RS.txt
+            fi
+        done < "$testcasesfile"
+        echo "Total number of passed cases: $passed" >> ../RESULT_RS.txt
     fi
 
     # Go
@@ -117,6 +214,24 @@ compile() {
         # Assume there is only one package in total
         # Compile Go sources
         go build -o Go/bin/Program `ls ./Go/*.go`
+        # create a new file for results
+        touch ../RESULT_GO.txt
+        # a counter keep track of number of test cases passed
+        passed=0
+        # compare answers and results
+        while read LINE
+        do
+            # save results for each test cases
+            ./Go/bin/Program $LINE > "res_${LINE// /-}"
+            # compare it with answer key
+            if [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}`=="" ]] || [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}_rev`=="" ]] ; then
+                # passed the test case
+                passed=$((passed+1))
+                # append result to result file
+                echo "Case: ${LINE// /-} passed" >> ../RESULT_GO.txt
+            fi
+        done < "$testcasesfile"
+        echo "Total number of passed cases: $passed" >> ../RESULT_GO.txt
     fi
 
     # Python
@@ -147,7 +262,28 @@ compile() {
         do
             mv ${cpp} Cpp/
         done
+        # assume using c++11
         g++ -g -std=c++11 `ls ./Cpp/*.cpp` -o ./Cpp/bin/Program
+
+        # create a new file for results
+        touch ../RESULT_CPP.txt
+        # a counter keep track of number of test cases passed
+        passed=0
+        # compare answers and results
+        while read LINE
+        do
+            # save results for each test cases
+            ./Cpp/bin/Program $LINE > "res_${LINE// /-}"
+            # compare it with answer key
+            if [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}`=="" ]] || [[ `diff -B res_${LINE// /-} ../../ANSWER_KEY/ans_${LINE// /-}_rev`=="" ]] ; then
+                # passed the test case
+                passed=$((passed+1))
+                # append result to result file
+                echo "Case: ${LINE// /-} passed" >> ../RESULT_CPP.txt
+            fi
+        done < "$testcasesfile"
+        echo "Total number of passed cases: $passed" >> ../RESULT_CPP.txt
+
     fi
 
     # TODO: add Java
